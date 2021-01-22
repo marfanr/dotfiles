104ae23c291fbfadcb484813d30d83333af5bdea
marfanr <mohammadarfan45@gmail.com>
2021-01-22 20:43:24 +0700

    first commit

---
+++ conky/.config/conky/main.lua
@@ -0,0 +1,21 @@
+do
+conky.config = {
+	alignment = 'br',
+	background = false,
+	own_window = true,
+	own_window_type = 'desktop',
+	own_window_transparent = true,
+	double_buffer = true,
+	gap_x = 60,
+	default_color = '#FFF',
+
+	use_xft = true,
+
+}
+
+
+conky.text = [[
+${font Noto Sans CJK JP:bold:size=35} 木曜日  ${font}
+
+]]
+end
